f3ea065395c632c44651cfffd35d1f6aab29346d
meets2310 <63064677+meets2310@users.noreply.github.com>
2026-03-13 01:53:53 +0000

    chore: update chat API endpoint from localhost to 'backend' hostname.

--- chat-widget.js
+++ chat-widget.js
@@ -400,7 +400,7 @@ TONE GUIDE:
     const typing = showTyping();
 
     try {
-      const res = await fetch('http://127.0.0.1:8000/chat', {
+      const res = await fetch('http://backend:8000/chat', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
